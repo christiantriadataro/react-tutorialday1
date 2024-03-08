d6385b2b2332406de53943c8e668217a7fee50dc
Christian Dataro <ultrainstinct2030@gmail.com>
2024-03-08 12:10:52 -0800

    Making a Custom Hook: Net Ninja

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './css/custom.css'
+import Navbar from "./components/Navbar";
+import Home from "./views/Home";
+function App() {
+  return (
+      <div className="App">
+          <Navbar />
+        <div className="content">
+            <Home />
+        </div>
+      </div>
+  );
+}
+
+export default App;
