2e3d5ed6aef88e7e00ac17cf27f9480b44101ccd
Christian Dataro <ultrainstinct2030@gmail.com>
2024-03-08 13:01:22 -0800

    useEffect camp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './css/custom.css'
 import Navbar from "./components/Navbar";
 import Home from "./views/Home";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Create from "./components/Create";
+
 function App() {
-  return (
-      <div className="App">
-          <Navbar />
-        <div className="content">
-            <Home />
-        </div>
-      </div>
-  );
+    return (
+        <Router>
+            <div className="App">
+                <Navbar/>
+                <div className="content">
+                    <Switch>
+                        <Route path="/create">
+                            <Create />
+                        </Route>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </div>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
