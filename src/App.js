222d0cf782ff7908617f946b53216bcff3f94835
Christian Dataro <58933979+christiantriadataro@users.noreply.github.com>
2024-03-10 00:30:22 -0800

    Completed Course

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import './css/custom.css'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from "./components/Navbar";
 import Home from "./views/Home";
-import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Create from "./components/Create";
+import BlogDetails from "./components/BlogDetails";
+import NotFound from "./views/NotFound";
+
 
 function App() {
     return (
         <Router>
             <div className="App">
-                <Navbar/>
+                <Navbar />
                 <div className="content">
                     <Switch>
+                        <Route exact path="/">
+                            <Home />
+                        </Route>
                         <Route path="/create">
                             <Create />
                         </Route>
-                        <Route exact path="/">
-                            <Home />
+                        <Route path="/blogs/:id">
+                            <BlogDetails />
+                        </Route>
+                        <Route path="*">
+                            <NotFound/>
                         </Route>
                     </Switch>
                 </div>
